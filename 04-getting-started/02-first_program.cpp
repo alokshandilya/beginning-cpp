617d04a739bd3669116bb02d336226f140f724d4
alokshandilya <alok.shandilya10@gmail.com>
2021-11-23 21:24:02 +0530

    add section 4

---
+++ 04-getting-started/02-first_program.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  int favouriteNumber;
+  std::cout << "Enter your favourite number b/w 1 and 100 : " << std::endl;
+  // in b/w try to print b\w :::: HINT -> use \\ (should be inside "")
+  std::cin >> favouriteNumber;
+  std::cout << "Amazing!! That's my favourite number too!!" << std::endl;
+  return 0;
+}
