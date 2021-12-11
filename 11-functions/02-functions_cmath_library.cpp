a0086141bc337e4330cc8cf21552aae8a2dbac3f
alokshandilya <alok.shandilya10@gmail.com>
2021-12-12 05:28:24 +0530

    consistent formatting | add output

--- 11-functions/02-functions_cmath_library.cpp
+++ 11-functions/02-functions_cmath_library.cpp
@@ -1,36 +1,48 @@
 // Section 11
 // Math examples
 
+#include <cmath> // required
 #include <iostream>
-#include <cmath>        // required
 
 using namespace std;
 
 int main() {
-    
-    double num {};
 
-    cout << "Enter a number (double) : ";
-    cin >> num;
-    
-    cout << "The sqrt of " << num << " is: " << sqrt(num) << endl;
-    cout << "The cubed root of " << num << " is: " << cbrt(num) << endl;
-    
-    cout << "The sine of " << num << " is: " << sin(num) << endl;
-    cout << "The cosine of " << num << " is: " << cos(num) << endl;
-    
-    cout << "The ceil of " << num << " is: " << ceil(num) << endl;
-    cout << "The floor of " << num << " is: " << floor(num) << endl;
-    cout << "The round of " << num << " is: " << round(num) << endl;
+  double num{};
 
-    
-    double power {};
-    cout << "\nEnter a power to raise " << num << " to: ";
-    cin >> power;
-    cout << num << " raised to the " << power << " power is: " << pow(num, power) << endl;
-    
+  cout << "Enter a number (double) : ";
+  cin >> num;
 
-    cout <<  endl;
-    return 0;
+  cout << "The sqrt of " << num << " is: " << sqrt(num) << endl;
+  cout << "The cubed root of " << num << " is: " << cbrt(num) << endl;
+
+  cout << "The sine of " << num << " is: " << sin(num) << endl;
+  cout << "The cosine of " << num << " is: " << cos(num) << endl;
+
+  cout << "The ceil of " << num << " is: " << ceil(num) << endl;
+  cout << "The floor of " << num << " is: " << floor(num) << endl;
+  cout << "The round of " << num << " is: " << round(num) << endl;
+
+  double power{};
+  cout << "\nEnter a power to raise " << num << " to: ";
+  cin >> power;
+  cout << num << " raised to the " << power << " power is: " << pow(num, power)
+       << endl;
+
+  cout << endl;
+  return 0;
 }
 
+// output
+//
+// Enter a number (double) : 77.77
+// The sqrt of 77.77 is: 8.81873
+// The cubed root of 77.77 is: 4.26845
+// The sine of 77.77 is: 0.696003
+// The cosine of 77.77 is: -0.718039
+// The ceil of 77.77 is: 78
+// The floor of 77.77 is: 77
+// The round of 77.77 is: 78
+//
+// Enter a power to raise 77.77 to: 2
+// 77.77 raised to the 2 power is: 6048.17
