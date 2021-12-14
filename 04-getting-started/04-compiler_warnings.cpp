5765cf981c61e224852564a5d81cb757d783fde7
alokshandilya <alok.shandilya10@gmail.com>
2021-12-14 23:24:12 +0530

    change indentwidth to 4

---
+++ 04-getting-started/04-compiler_warnings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int myFavouriteNumber;
+    std::cout << "Hello!! World" << std::endl;
+    return 0;
+}
+
+// compiler warnings
+//
+// 04-compiler_warnings.cpp: In function 'int main()':
+// 04-compiler_warnings.cpp:4:7: warning: unused variable 'myFavouriteNumber'
+// [-Wunused-variable]
+//     4 |   int myFavouriteNumber;
+//       |       ^~~~~~~~~~~~~~~~~
---
+++ 04-getting-started/05-linker_error.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+extern int x;
+// When we define a variable as extern , we tell the compiler to assume the
+// variable bas been defined somewhere else and let the linker worry about
+// finding it.
+
+int main() {
+    std::cout << "Hello!! World" << std::endl;
+    std::cout << x;
+    return 0;
+}
+
+// error (Linker error)
+//
+// 5-linkerErrors.cpp:(.text+0x31): undefined reference to `x'
